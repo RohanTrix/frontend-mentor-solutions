43903d098839e4e1897b8b55d9aedbd00813d021
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2023-04-14 02:08:26 +0530

    Inital Commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <main class="h-screen grid place-content-center bg-[#121417]">
+    <div class="flex flex-col mx-7 p-4 text-slate-400 bg-[#18212E] rounded-3xl max-w-[430px] radial-gradient">
+      <div v-if="!isFormSubmitted" class="mx-3">
+
+        <div class="flex flex-col mt-4">
+
+          <div class="rounded-full h-10 w-10 bg-[#959eac40] grid place-content-center mb-5">
+            <img src="./assets/images/icon-star.svg" alt="Star" class="aspect-square h-4">
+          </div>
+
+          <h1 class="text-white text-2xl font-overpass font-medium">How did we do?</h1>
+          <p class="text-[#959eac] font-overpass py-3">Please let us know how we did with your support request. All
+            feedback is
+            appreciated to help us improve our offering!</p>
+
+        </div>
+
+        <RatingForm @submitForm="submitFormHandler" />
+
+
+      </div>
+      <div v-else class="mx-3">
+        <div class="flex flex-col items-center">
+          <img src="./assets/images/illustration-thank-you.svg" alt="Thank You" class="my-5">
+          <p class="text-[#fb7413] text-center rounded-full bg-[#959eac40] px-5 py-1 my-2">You selected {{ ratingScore }}
+            out of 5</p>
+          <h1 class="text-white text-2xl font-overpass font-medium mt-4">Thank You!</h1>
+          <p class="text-center px-2 mt-1 mb-3">We appreciate you taking the time to give a rating. If you ever need more
+            support,
+            don't hesitate to get in touch!</p>
+
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CircleContainer from './components/CircleContainer.vue';
+import RatingForm from './components/RatingForm.vue';
+
+const isFormSubmitted = ref(false)
+const ratingScore = ref(0)
+const submitFormHandler = (choice) => {
+  isFormSubmitted.value = true;
+  ratingScore.value = choice
+}
+</script>
+
+<style scoped>
+.radial-gradient {
+  background: radial-gradient(at top,
+      #212832 10%, #18212E);
+}
+</style>
---
+++ src/components/RatingForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="$emit('submitForm', choice)" class="flex flex-col items-center w-full">
+
+
+        <div class="flex items-center justify-between w-full px-1 my-3 flex-wrap gap-y-2">
+            <div v-for="n in 5" :key="n">
+                <input type="radio" v-model="choice" name="rating" :value="n" :id="`score${n}`" class="hidden" />
+                <label :for="`score${n}`">
+                    <div :class="choice == n ? 'text-white bg-[#fb7413] hover:bg-none active:bg-none' : 'hover:bg-[#959eac]'"
+                        class="rounded-full h-10 w-10 bg-[#959eac40] grid place-content-center hover:text-white transition-all duration-300">
+                        <p class="font-bold">{{ n }}</p>
+                    </div>
+                </label>
+
+            </div>
+
+
+        </div>
+        <button type="submit" :disabled="choice === null"
+            class="p-2 w-full my-3 flex justify-center items-center gap-2 text-center bg-[#fb7413] text-white font-medium rounded-full uppercase tracking-[0.1rem]
+                      hover:bg-white hover:text-[#fb7413] transition-all duration-300 disabled:bg-[#959eac] disabled:hover:text-white">
+            Submit
+        </button>
+    </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CircleContainer from './CircleContainer.vue';
+
+const choice = ref(null)
+
+const submitForm = () => { }
+</script>
+
+<style scoped></style>
